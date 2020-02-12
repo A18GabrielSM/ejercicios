3f037675b1c9f05fb2d787f89e84a2507cd8d3c4
Gabriel Sanchez <a18gabrielsm@iessanclemente.net>
2020-02-12 12:01:41 +0100

    Practica Modelos

--- Chapter06/r10_sorting_recordset/my_library/models/library_book.py
+++ Chapter06/r10_sorting_recordset/my_library/models/library_book.py
@@ -18,14 +18,13 @@ class LibraryBook(models.Model):
     date_updated = fields.Datetime('Last Updated', copy=False)
     author_ids = fields.Many2many('res.partner', string='Authors')
     category_id = fields.Many2one('library.book.category', string='Category')
+    member_id = fields.Many2one('library.member', string='Lector')
     state = fields.Selection([
         ('draft', 'Unavailable'),
         ('available', 'Available'),
         ('borrowed', 'Borrowed'),
         ('lost', 'Lost')],
         'State', default="draft")
-
-    #Meter el metodo aqui
 
     @api.model
     def is_allowed_transition(self, old_state, new_state):
@@ -34,7 +33,9 @@ class LibraryBook(models.Model):
                    ('borrowed', 'available'),
                    ('available', 'lost'),
                    ('borrowed', 'lost'),
-                   ('lost', 'available')]
+                   ('lost', 'available'),
+                   #Cambio a metodo
+                   ('available','draft')]
         return (old_state, new_state) in allowed
 
     @api.multi
@@ -43,11 +44,15 @@ class LibraryBook(models.Model):
             if book.is_allowed_transition(book.state, new_state):
                 book.state = new_state
             else:
-                message = _('Moving from %s to %s is not allowd') % (book.state, new_state)
+                message = _('Moving from %s to %s is not allowed') % (book.state, new_state)
                 raise UserError(message)
 
     def make_available(self):
         self.change_state('available')
+
+    #Metodo ejercicio:
+    def make_unavailable(self):
+        self.change_state('draft')
 
     def make_borrowed(self):
         self.change_state('borrowed')
