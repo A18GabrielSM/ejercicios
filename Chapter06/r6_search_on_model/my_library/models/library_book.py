a8b2ef43bf204811e2de00fa6ad1a5c720a9956e
Gabriel Sanchez <a18gabrielsm@iessanclemente.net>
2020-02-14 12:54:14 +0100

    Commit inicial de practica 2

--- Chapter06/r6_search_on_model/my_library/models/library_book.py
+++ Chapter06/r6_search_on_model/my_library/models/library_book.py
@@ -108,3 +108,15 @@ class LibraryMember(models.Model):
     date_end = fields.Date('Termination Date')
     member_number = fields.Char()
     date_of_birth = fields.Date('Date of birth')
+
+class LibraryLoan(models.Model):
+    _name = 'library.loan'
+    _rec_name = 'book_id'
+
+    member_id = fields.Many2one('library.member', required=True)
+    book_id = fields.Many2one('library.book', required=True)
+    date_start = fields.Date('Fecha Inicio', default=datetime.date.today())
+    date_end = fields.Date('Fecha Fin', default=datetime.date.today() + datetime.timedelta(days=5))
+    member_image = fields.Binary('Member Image', related='member_id.partner_id.image')
+    _order = 'date_end desc'
+
